e0cd92bd45992ace7dcb5a3284476cc643bce4c9
Mantas Pucka <mantas@8devices.com>
2015-10-02 13:30:18 +0300

    mips: atomic: simplify atomic.h just to allow compile UBIFS
    It needs review for correctness

--- arch/mips/include/asm/atomic.h
+++ arch/mips/include/asm/atomic.h
@@ -11,186 +11,58 @@
  *
  * Copyright (C) 1996, 97, 99, 2000, 03, 04, 06 by Ralf Baechle
  */
-#ifndef _ASM_ATOMIC_H
-#define _ASM_ATOMIC_H
+#ifndef _ASM_MIPS_ATOMIC_H
+#define _ASM_MIPS_ATOMIC_H
 
-// #include <linux/irqflags.h>
+#ifdef CONFIG_SMP
+#error SMP not supported
+#endif
+
 #include <linux/types.h>
-// #include <asm/barrier.h>
-// #include <asm/compiler.h>
-// #include <asm/cpu-features.h>
-// #include <asm/cmpxchg.h>
-// #include <asm/war.h>
-//#define atomic_t unsigned long
 
-typedef struct {
-	int counter;
-} atomic_t;
+typedef struct { volatile int counter; } atomic_t;
+
 #define ATOMIC_INIT(i)	  { (i) }
 
-/*
- * atomic_read - read atomic variable
- * @v: pointer of type atomic_t
- *
- * Atomically reads the value of @v.
- */
-#define atomic_read(v)		ACCESS_ONCE((v)->counter)
-
-/*
- * atomic_set - set atomic variable
- * @v: pointer of type atomic_t
- * @i: required value
- *
- * Atomically sets the value of @v to @i.
- */
+#define atomic_read(v)		((v)->counter)
 #define atomic_set(v, i)		((v)->counter = (i))
 
-#define ATOMIC_OP(op, c_op, asm_op)					      \
-static __inline__ void atomic_##op(int i, atomic_t * v)			      \
-{									      \
-		v->counter c_op i;					      \
-}
+//TODO is this safe for U-Boot? (IRQs are disabled, right?)
 
-#define ATOMIC_OP_RETURN(op, c_op, asm_op)				      \
-static __inline__ int atomic_##op##_return(int i, atomic_t * v)		      \
-{									      \
-	int result;							      \
-									      \
-	result = v->counter;						      \
-	result c_op i;							      \
-	v->counter = result;						      \
-									      \
-	return result;							      \
-}
-
-#define ATOMIC_OPS(op, c_op, asm_op)					      \
-	ATOMIC_OP(op, c_op, asm_op)					      \
-	ATOMIC_OP_RETURN(op, c_op, asm_op)
-
-ATOMIC_OPS(add, +=, addu)
-ATOMIC_OPS(sub, -=, subu)
-
-#undef ATOMIC_OPS
-#undef ATOMIC_OP_RETURN
-#undef ATOMIC_OP
-
-/*
- * atomic_sub_if_positive - conditionally subtract integer from atomic variable
- * @i: integer value to subtract
- * @v: pointer of type atomic_t
- *
- * Atomically test @v and subtract @i if @v is greater or equal than @i.
- * The function returns the old value of @v minus @i.
- */
-static __inline__ int atomic_sub_if_positive(int i, atomic_t * v)
+static inline void atomic_add(int i, volatile atomic_t *v)
 {
-	int result;
+// 	unsigned long flags;
 
-	unsigned long flags;
-
-// 	raw_local_irq_save(flags);
-	result = v->counter;
-	result -= i;
-	if (result >= 0)
-		v->counter = result;
-// 	raw_local_irq_restore(flags);
-
-	return result;
+// 	local_irq_save(flags);
+	v->counter += i;
+// 	local_irq_restore(flags);
 }
 
-#define atomic_cmpxchg(v, o, n) (cmpxchg(&((v)->counter), (o), (n)))
-#define atomic_xchg(v, new) (xchg(&((v)->counter), (new)))
-
-/**
- * __atomic_add_unless - add unless the number is a given value
- * @v: pointer of type atomic_t
- * @a: the amount to add to v...
- * @u: ...unless v is equal to u.
- *
- * Atomically adds @a to @v, so long as it was not @u.
- * Returns the old value of @v.
- */
-static __inline__ int __atomic_add_unless(atomic_t *v, int a, int u)
+static inline void atomic_sub(int i, volatile atomic_t *v)
 {
-	int c, old;
-	c = atomic_read(v);
-	for (;;) {
-		if (unlikely(c == (u)))
-			break;
-		old = atomic_cmpxchg((v), c, c + (a));
-		if (likely(old == c))
-			break;
-		c = old;
-	}
-	return c;
+// 	unsigned long flags;
+
+// 	local_irq_save(flags);
+	v->counter -= i;
+// 	local_irq_restore(flags);
 }
 
-#define atomic_dec_return(v) atomic_sub_return(1, (v))
-#define atomic_inc_return(v) atomic_add_return(1, (v))
+static inline void atomic_inc(volatile atomic_t *v)
+{
+// 	unsigned long flags;
 
-/*
- * atomic_sub_and_test - subtract value from variable and test result
- * @i: integer value to subtract
- * @v: pointer of type atomic_t
- *
- * Atomically subtracts @i from @v and returns
- * true if the result is zero, or false for all
- * other cases.
- */
-#define atomic_sub_and_test(i, v) (atomic_sub_return((i), (v)) == 0)
+// 	local_irq_save(flags);
+	v->counter += 1;
+// 	local_irq_restore(flags);
+}
 
-/*
- * atomic_inc_and_test - increment and test
- * @v: pointer of type atomic_t
- *
- * Atomically increments @v by 1
- * and returns true if the result is zero, or false for all
- * other cases.
- */
-#define atomic_inc_and_test(v) (atomic_inc_return(v) == 0)
+static inline void atomic_dec(volatile atomic_t *v)
+{
+// 	unsigned long flags;
 
-/*
- * atomic_dec_and_test - decrement by 1 and test
- * @v: pointer of type atomic_t
- *
- * Atomically decrements @v by 1 and
- * returns true if the result is 0, or false for all other
- * cases.
- */
-#define atomic_dec_and_test(v) (atomic_sub_return(1, (v)) == 0)
+// 	local_irq_save(flags);
+	v->counter -= 1;
+// 	local_irq_restore(flags);
+}
 
-/*
- * atomic_dec_if_positive - decrement by 1 if old value positive
- * @v: pointer of type atomic_t
- */
-#define atomic_dec_if_positive(v)	atomic_sub_if_positive(1, v)
-
-/*
- * atomic_inc - increment atomic variable
- * @v: pointer of type atomic_t
- *
- * Atomically increments @v by 1.
- */
-#define atomic_inc(v) atomic_add(1, (v))
-
-/*
- * atomic_dec - decrement and test
- * @v: pointer of type atomic_t
- *
- * Atomically decrements @v by 1.
- */
-#define atomic_dec(v) atomic_sub(1, (v))
-
-/*
- * atomic_add_negative - add and test if negative
- * @v: pointer of type atomic_t
- * @i: integer value to add
- *
- * Atomically adds @i to @v and returns true
- * if the result is negative, or false when
- * result is greater than or equal to zero.
- */
-#define atomic_add_negative(i, v) (atomic_add_return(i, (v)) < 0)
-
-
-#endif /* _ASM_ATOMIC_H */
+#endif /* _ASM_MIPS_ATOMIC_H */
